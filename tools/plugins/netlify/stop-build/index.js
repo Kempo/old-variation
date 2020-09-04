b7c905c0a953dfcc4c5a71913efbf29483ec5117
Aaron <ilestkempo@gmail.com>
2020-09-03 18:09:20 -0700

    added stop-build plugin for netlify

---
+++ tools/plugins/netlify/stop-build/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  onPreBuild: ({ utils }) => {
+    // Set within Netlify for each site.
+    const projectName = process.env.PROJECT_NAME;
+
+    const latestCommit = 'HEAD'; // the current commit we're building
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const url = process.env.REPOSITORY_URL;
+
+    const isProjectChanged = projectChanged(projectName, lastDeployedCommit, latestCommit);
+    console.log(`Current commit: ${process.env.COMMIT_REF}`);
+    console.log(`Previous deployed commit: ${process.env.CACHED_COMMIT_REF}`);
+
+    if(!isProjectChanged) {
+      utils.build.cancelBuild(`Cancelling build because the current '${projectName}' website is unaffected. \n REPOSITORY_URL: ${url} `);
+    }
+  }
+}
+
+function projectChanged(currentProject, fromHash, toHash) {
+  const execSync = require('child_process').execSync;
+  const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+  const output = execSync(getAffected).toString();
+
+  const changedProjects = JSON.parse(output).projects;
+  console.log(`Changed Projects: ${changedProjects}`)
+
+  return changedProjects.some(project => project === currentProject);
+}
